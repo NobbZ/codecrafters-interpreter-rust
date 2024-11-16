2f646562ab94f868f9bbace8aac6c68be62c3903
Norbert Melzer <timmelzer@gmail.com>
2024-11-17 00:33:05 +0100

    fix exit code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 mod interface;
 mod token;
 
-use anyhow::Result;
+use std::process::ExitCode;
 
 use interface::{InterpreterCommand, InterpreterParser, TokenizeArgs};
 
-fn main() -> Result<(), usize> {
+fn main() -> ExitCode {
     let args = <InterpreterParser as clap::Parser>::parse();
 
     let ec = match args.command {
         InterpreterCommand::Tokenize(tok_args) => token::tokenize(tok_args).map_or(65, |_| 0),
     };
 
-    if ec == 0 {
-        Ok(())
-    } else {
-        Err(ec)
-    }
+    ExitCode::from(ec)
 }
