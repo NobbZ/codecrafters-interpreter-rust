740c61a65bf53c90a0dc663859b1edeb50eb20c4
Norbert Melzer <timmelzer@gmail.com>
2024-11-18 11:37:38 +0100

    add codecrafters CLI to dev env

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         devShells.default = pkgs.mkShell {
           packages = builtins.attrValues {
             inherit (pkgs) cargo-nextest cargo-audit cargo-deny cargo-tarpaulin rust-analyzer;
-            inherit (pkgs) nil pre-commit reuse;
+            inherit (pkgs) nil pre-commit reuse codecrafters-cli;
             inherit (pkgs) watchexec;
             inherit rust;
           };
